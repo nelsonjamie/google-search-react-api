21fb3b3541183223ffc4b1b39279a8b9be2325a0
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-13 10:27:31 +0700

    Created get controller

--- controllers/search.js
+++ controllers/search.js
@@ -4,6 +4,9 @@ const router = express.Router()
 
 // Views
 // Create here a controller that accepts GET requests and renders the "search" page
+app.get('/', (req, res) => {
+  res.render('search')
+})
 
 // Export
 module.exports = router
