4f204e0be8e1ff54d9abe30a3cb2d17c707b1587
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-13 11:12:17 +0700

    Reverted to previous version

--- controllers/results.js
+++ controllers/results.js
@@ -3,8 +3,5 @@ const express = require('express')
 const router = express.Router()
 
 // Create POST controller
-app.POST('/results', (req, res) => {
-  res.render('results')
-})
 
 // Export module
