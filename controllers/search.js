e4556405c1bfc62e7e3c6fe1ccf1c1f2250f974a
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-13 11:12:33 +0700

    Reverted to previous version

--- controllers/search.js
+++ controllers/search.js
@@ -4,10 +4,6 @@ const router = express.Router()
 
 // Views
 // Create here a controller that accepts GET requests and renders the "search" page
-const searchControl = app.get('/', (req, res) => {
-  res.render('search')
-})
 
 // Export
 module.exports = router
-module.exports = searchControl
