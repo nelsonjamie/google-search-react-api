da9692d1d17e30db66574e17f6e0e360093b566a
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-13 10:27:10 +0700

    Created post controller

--- controllers/results.js
+++ controllers/results.js
@@ -1,5 +1,10 @@
 // Import Packages
+const express = require('express')
+const router = express.Router()
 
 // Create POST controller
+app.POST('/results', (req, res) => {
+  res.render('results')
+})
 
 // Export module
