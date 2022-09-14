ec516e7999eeaef0861a15a42e7ae3e351b57520
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-14 12:45:28 +0700

    Removed view

--- controllers/search.js
+++ controllers/search.js
@@ -2,10 +2,6 @@
 const express = require('express')
 const router = express.Router()
 
-// Views
-app.use('/', (req, res) => {
-  res.render('search')
-})
 // Create here a controller that accepts GET requests and renders the "search" page
 router.get('/', (req, res) => {
   res.render('search')
