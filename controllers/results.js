00cb430370741f6f0c0fe9a2e8dfb2cad5f4d75f
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-14 12:46:45 +0700

    Removed post controller, added results array

--- controllers/results.js
+++ controllers/results.js
@@ -2,15 +2,60 @@
 const express = require('express')
 const router = express.Router()
 
-//Views
+// Create POST controller
 router.get('/', (req, res) => {
   res.render('results')
 })
 
-// Create POST controller
-router.POST('/', (req, res) => {
-  res.render('results')
-})
+//Results Array
+
+let resArr = [
+  {
+    title: 'JS tutorials',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js'
+      },
+      {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js'
+      }
+    ]
+  },
+  {
+    title: 'Test 2',
+    description: 'stuff2',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: '2222',
+        url: 'https://www.w3schools.com/js'
+      },
+      {
+        title: '22222',
+        url: 'https://www.w3schools.com/js'
+      }
+    ]
+  },
+  {
+    title: 'Test 3',
+    description: 'stuff3',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: '3333',
+        url: 'https://www.w3schools.com/js'
+      },
+      {
+        title: '33333',
+        url: 'https://www.w3schools.com/js'
+      }
+    ]
+  }
+]
 
 // Export module
 module.exports = router
