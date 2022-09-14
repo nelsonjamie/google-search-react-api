fb9acab9722c2a9defa4d1b612ecad0aa912c6c1
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-14 12:17:10 +0700

    Added view and controller

--- controllers/search.js
+++ controllers/search.js
@@ -3,7 +3,13 @@ const express = require('express')
 const router = express.Router()
 
 // Views
+app.use('/', (req, res) => {
+  res.render('search')
+})
 // Create here a controller that accepts GET requests and renders the "search" page
+router.get('/', (req, res) => {
+  res.render('search')
+})
 
 // Export
 module.exports = router
